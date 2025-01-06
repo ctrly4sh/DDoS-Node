c9b9d09d84150e8f3ff125e504e8ac61db168d53
ctrly4sh <149458186+ctrly4sh@users.noreply.github.com>
2025-01-06 22:07:10 +0530

    express server

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import express , {Request, Response} from "express";
+
+const app = express();
+
+app.get('/health' , (req: Request , res: Response) => {
+   res.status(200).json({
+    success: true,
+    Message: "server health is okay"
+  })
+})
+
+const PORT= process.env.PORT || 8001 ;
+app.listen(PORT, () => {
+  console.log(`Server listening at localhost:${PORT}`)
+})
